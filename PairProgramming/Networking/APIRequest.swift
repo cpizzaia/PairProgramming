d14cb4b38530e716ad52d2ab0db179ae6c0853f7
Cody Pizzaia <cody@pizzaia.com>
2019-10-31 12:01:53 -0400

    cleanup project

--- PairProgramming/Networking/APIRequest.swift
+++ PairProgramming/Networking/APIRequest.swift
@@ -11,41 +11,20 @@ import Alamofire
 import SwiftyJSON
 
 class APIRequest {
+  // MARK: Public Types
+  typealias JSONResponse = (JSON) -> Void
+  typealias ErrorResponse = (APIError) -> Void
 
   struct APIError: Error {
     let code: Int
-    let domainCode: Int?
-    let errorDescription: String
-    let validationDetails: String?
-    let userTitle: String?
-    let userMessage: String?
-
-    var title: String {
-      get {return userTitle ?? "Error"}
-    }
-
-    var description: String {
-      get {return userMessage ?? validationDetails ?? errorDescription}
-    }
-
-    init(code: Int, domainCode: Int? = nil, description: String, validationDetails: String? = nil, userTitle: String? = nil, userMessage: String? = nil) {
-      self.code = code
-      self.domainCode = domainCode
-      self.errorDescription = description
-      self.validationDetails = validationDetails
-      self.userTitle = userTitle
-      self.userMessage = userMessage
-    }
+    let body: JSON
   }
 
   struct Response {
     let statusCode: Int
-    let headers: Headers
-    let body: Body
+    let headers: JSON
+    let body: JSON
   }
-
-  typealias Headers = JSON
-  typealias Body = JSON
 
   struct RequestParams {
     let url: String
@@ -53,41 +32,35 @@ class APIRequest {
     let body: Parameters?
     let headers: HTTPHeaders?
     let success: JSONResponse
-    let failure: JSONResponse
+    let failure: ErrorResponse
   }
 
-  typealias JSONResponse = (JSON) -> Void
-
-  func get(url: String, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping JSONResponse) {
-
+  // MARK: Public Methods
+  func get(url: String, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping ErrorResponse) {
     request(params: RequestParams(url: url, method: .get, body: nil, headers: headers, success: success, failure: failure))
 
   }
 
-  func post(url: String, body: Parameters?, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping JSONResponse) {
-
+  func post(url: String, body: Parameters?, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping ErrorResponse) {
     request(params: RequestParams(url: url, method: .post, body: body, headers: headers, success: success, failure: failure))
 
   }
-
-  func delete(url: String, body: Parameters?, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping JSONResponse) {
-
+  func delete(url: String, body: Parameters?, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping ErrorResponse) {
     request(params: RequestParams(url: url, method: .delete, body: body, headers: headers, success: success, failure: failure))
 
   }
 
-  func patch(url: String, body: Parameters?, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping JSONResponse) {
-
+  func patch(url: String, body: Parameters?, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping ErrorResponse) {
     request(params: RequestParams(url: url, method: .patch, body: body, headers: headers, success: success, failure: failure))
 
   }
 
-  func put(url: String, body: Parameters?, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping JSONResponse) {
-
+  func put(url: String, body: Parameters?, headers: HTTPHeaders?, success: @escaping JSONResponse, failure: @escaping ErrorResponse) {
     request(params: RequestParams(url: url, method: .put, body: body, headers: headers, success: success, failure: failure))
 
   }
 
+  // MARK: Private Methods
   private func request(params: RequestParams) {
     log("APIRequest -> \(params.method.rawValue): \(params.url)")
 
@@ -99,13 +72,11 @@ class APIRequest {
       headers: params.headers
       ).validate().responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
         self.handle(response: response, params: params)
-      })
-
+      }
+    )
   }
 
-  // MARK: Private Methods
   private func handle(response: DataResponse<Any>, params: RequestParams) {
-
     let json: JSON
 
     do {
@@ -115,20 +86,14 @@ class APIRequest {
       json = JSON()
     }
 
-    let apiResponse = Response(
-      statusCode: response.response?.statusCode ?? 400,
-      headers: JSON(response.response?.allHeaderFields ?? [:]),
-      body: json
-    )
-
     switch response.result {
     case .success:
       log("APIRequest -> SUCCESS: \(params.url)")
-      params.success(apiResponse.body)
+      params.success(json)
 
     case .failure:
       log("APIRequest -> FAILURE: \(params.url)")
-      params.failure(apiResponse.body)
+      params.failure(.init(code: response.response?.statusCode ?? 400, body: json))
     }
   }
 }
